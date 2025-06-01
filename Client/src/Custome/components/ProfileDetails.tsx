ad408d79155ebec3260c31a6b7a94fc1980ae44c
Hyacinth Augusting <augustinehyacinth3@gmail.com>
2025-06-01 21:01:00 +0100

    updating the project folders both forntend and backend

--- Client/src/Custome/components/ProfileDetails.tsx
+++ Client/src/Custome/components/ProfileDetails.tsx
@@ -1,9 +1,19 @@
+import { faker } from "@faker-js/faker";
+
+const randomName = faker.person.fullName();
+const avatar = faker.image.avatar();
+
 const ProfileDetails = () => {
   return (
-    <div className="flex gap-2 items-center">
-      {/* show profile  or registration name */}
-      <img src="random" alt="" className="size-14 rounded-full  border-2 " />
-      <h2> profile name</h2>
+    <div className="flex gap-0 items-center flex-col">
+      <img
+        src={avatar}
+        alt={`${randomName} image`}
+        className="size-14 rounded-full  border-2 "
+      />
+      <h2 className="font-Merriweather font-semibold text-gray-900">
+        {randomName}
+      </h2>
     </div>
   );
 };
